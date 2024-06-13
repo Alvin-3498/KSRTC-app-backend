f48cfa57810b24460f3ca85177e98daa41464f6d
Alvin-3498 <alvinkonukudy8@gmail.com>
2024-06-14 00:25:37 +0530

    Search Completed

--- app.js
+++ app.js
@@ -17,6 +17,20 @@ app.post("/register",(req,res) => {
     res.json({"status":"Success"})
 })
 
+app.post("/search",(req,res) => {
+    let input = req.body
+    ksrtcmodel.find(input).then(
+        (data) => {
+            res.json(data)
+        }
+    ).catch(
+        (error) => {
+            res.json(error)
+        }
+    ).finally()
+})
+
+
 app.listen(8081,()=>{
     console.log("Server is active")
 })
